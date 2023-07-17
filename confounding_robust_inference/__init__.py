e515c66fcb6771aa412546c0c939e3e3e3deae01
Kei Ishikawa <k.stoneriv3@gmail.com>
2023-07-17 20:34:49 +0100

    import submodules when top-level module is imported

---
+++ confounding_robust_inference/__init__.py
@@ -0,0 +1,4 @@
+from confounding_robust_inference import data, estimators, policies, utils
+from confounding_robust_inference.version import __version__
+
+__all__ = ["data", "estimators", "policies", "utils", "__version__"]
